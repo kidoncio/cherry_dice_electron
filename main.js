c09238cde554cf1824c465f396d6d5bd27a24e76
Kidoncio <lucas.kidoncio@gmail.com>
2019-11-14 17:37:21 -0300

    Ajustado para remover o Menu antes de criar o Menu

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 require('v8-compile-cache');
 
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
+
+Menu.setApplicationMenu(null)
 
 function createWindow () {
   // Create the browser window.
